799fdd38cb1fe8ab5f87f9786ff5402630882fd2
Brigitta Bódi <brigi9222@gmail.com>
2021-09-19 13:46:43 +0200

    main

---
+++ src/test/java/com/example/codecooladvancednurtrybackend/CodecoolAdvancedNurtryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecooladvancednurtrybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecoolAdvancedNurtryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
