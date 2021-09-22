7f8cb03e8784234b31ec0e554397c176568014a8
Brigitta Bódi <brigi9222@gmail.com>
2021-09-22 12:29:01 +0200

    handle cross origin resource

--- src/main/java/com/example/nutry/controller/ResultController.java
+++ src/main/java/com/example/nutry/controller/ResultController.java
@@ -4,6 +4,7 @@ import com.example.nutry.model.Food;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class ResultController {
     @Value("${api.key}")
     private String apiKey;
