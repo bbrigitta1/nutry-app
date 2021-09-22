47e6bec8b4ee17b5d4e3e4ac5c96981ffa2be33c
Brigitta Bódi <brigi9222@gmail.com>
2021-09-22 17:20:31 +0200

    create user object

---
+++ src/main/java/com/example/nutry/model/User.java
@@ -0,0 +1,48 @@
+package com.example.nutry.model;
+
+import javax.persistence.*;
+
+//username: "",
+//    age: "",
+//    weight: "",
+//    height: "",
+//    gender: "",
+//    activity: "",
+//    goal: "",
+//    recommended: "",
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name="username")
+    private String userName;
+
+    @Column(name="age")
+    private int age;
+
+    @Column(name="weight")
+    private int weight;
+
+    @Column(name="height")
+    private int height;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="activity")
+    private String activity;
+
+    @Column(name="goal")
+    private String goal;
+
+    @Column(name="recommended")
+    private int recommended;
+
+    public User() {
+    }
+}
