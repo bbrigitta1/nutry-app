852f75cbe09108daa48b267d279a4cd3e494799e
Brigitta Bódi <brigi9222@gmail.com>
2021-09-28 11:15:58 +0200

    remove unnecessary insert

--- src/main/resources/nutry.sql
+++ src/main/resources/nutry.sql
@@ -4,8 +4,3 @@ CREATE TABLE foods (
     id serial PRIMARY KEY,
     description varchar(255)
 );
-
-INSERT INTO foods(description) VALUES('Apple');
-INSERT INTO foods(description) VALUES('Egg');
-INSERT INTO foods(description) VALUES('Bread');
-INSERT INTO foods(description) VALUES('Cherry');
