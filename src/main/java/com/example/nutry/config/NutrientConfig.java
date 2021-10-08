f23dd975462359846e0ca54c19b06ebbe6b846ae
Brigitta Bódi <brigi9222@gmail.com>
2021-10-08 23:50:22 +0200

    init macrinutrients

--- src/main/java/com/example/nutry/config/NutrientConfig.java
+++ src/main/java/com/example/nutry/config/NutrientConfig.java
@@ -23,7 +23,29 @@ public class NutrientConfig {
                     .unitName("MG")
                     .build();
 
+            Nutrient nutrient2 = Nutrient.builder()
+                    .nutrientId2(1003L)
+                    .nutrientName("Protein")
+                    .unitName("G")
+                    .build();
+
+            Nutrient nutrient3 = Nutrient.builder()
+                    .nutrientId2(1004L)
+                    .nutrientName("Total lipid (fat)")
+                    .unitName("G")
+                    .build();
+
+            Nutrient nutrient4 = Nutrient.builder()
+                    .nutrientId2(1005L)
+                    .nutrientName("Carbohydrate, by difference")
+                    .unitName("MG")
+                    .build();
+
             nutrientService.save(nutrient1);
+            nutrientService.save(nutrient2);
+            nutrientService.save(nutrient3);
+            nutrientService.save(nutrient4);
+
         };
     };
 };
