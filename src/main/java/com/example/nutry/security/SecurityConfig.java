2e6d37c187c64413b878414bb19b3d3365f5034b
Kovacs Tamas <kovacstamas990@gmail.com>
2021-10-16 17:47:26 +0200

    Endpoints are added to securityconfig

--- src/main/java/com/example/nutry/security/SecurityConfig.java
+++ src/main/java/com/example/nutry/security/SecurityConfig.java
@@ -33,6 +33,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll() // allowed by anyone
+                .antMatchers("/updatemealplan").permitAll() // allowed by anyone
+                .antMatchers("/addfoodtomealplan").permitAll() // allowed by anyone
+                .antMatchers("/getenergyhistory").permitAll() // allowed by anyone
                 .antMatchers(HttpMethod.GET, "/nutritiondetails").permitAll() // allowed only when signed in
 //                .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
                 .anyRequest().denyAll() // anything else is denied
