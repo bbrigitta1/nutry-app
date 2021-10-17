1cca1ff7d852cb3d0e5edd61f122cfedf9ea5bd4
Brigitta Bódi <brigi9222@gmail.com>
2021-10-17 13:49:47 +0200

    cors issue

--- src/main/java/com/example/nutry/security/SecurityConfig.java
+++ src/main/java/com/example/nutry/security/SecurityConfig.java
@@ -26,17 +26,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/").permitAll() // allowed by anyone
-                .antMatchers("/updatemealplan").permitAll() // allowed by anyone
-                .antMatchers("/addfoodtomealplan").permitAll() // allowed by anyone
-                .antMatchers("/getenergyhistory").permitAll() // allowed by anyone
-                .antMatchers(HttpMethod.GET, "/nutritiondetails").permitAll() // allowed only when signed in
+
+                .antMatchers(HttpMethod.POST, "/updatemealplan").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.GET, "/updatemealplan").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.POST, "/addfoodtomealplan").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.GET, "/addfoodtomealplan").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.GET, "/getenergyhistory").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.POST, "/getenergyhistory").permitAll() // allowed by anyone
+//                .antMatchers(HttpMethod.GET, "http://localhost:3000/nutrition-details").permitAll() // allowed only when signed in
+//                .antMatchers(HttpMethod.POST, "http://localhost:3000/nutrition-details").permitAll() // allowed only when signed in
 //                .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
                 .anyRequest().denyAll() // anything else is denied
                 .and()
