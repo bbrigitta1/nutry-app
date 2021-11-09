ce90690f522e82571d4a57abef7a4af74abdacfa
Kovacs Tamas <kovacstamas990@gmail.com>
2021-11-09 22:01:55 +0100

    Crossorigin is set in selectednutrients controller

--- src/main/java/com/example/nutry/controller/SelectedNutrientsController.java
+++ src/main/java/com/example/nutry/controller/SelectedNutrientsController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="http://localhost:3000")
+@CrossOrigin(origins="${crossorigin}")
 public class SelectedNutrientsController {
     
 
