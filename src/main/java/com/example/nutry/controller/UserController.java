ae40156061ee5eb13252a6acf77dcc59a6022f2c
Kovacs Tamas <kovacstamas990@gmail.com>
2021-11-09 22:51:44 +0100

    File directory is changed

--- src/main/java/com/example/nutry/controller/UserController.java
+++ src/main/java/com/example/nutry/controller/UserController.java
@@ -53,7 +53,7 @@ public class UserController {
 
     private UUID uuid;
 
-    private static String imageDirectory = System.getProperty("java.io.tmpdir") + "/profileimages";
+    private static String imageDirectory = System.getProperty("user.dir") + "/profileimages";
 
     @PostMapping("/addusertodatabase")
     public void saveUser(@RequestBody UserRegistrationDTO userDTO){
